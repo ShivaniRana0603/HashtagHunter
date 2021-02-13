8036d401a1097824b6eddcfd27fbae3e69c3ce59
Rana Shivani <shivanirana371@gmail.com>
2021-02-13 15:22:52 +0530

    added search bar design

--- app/src/main/java/com/example/hashtaghunter/ProfileActivity.java
+++ app/src/main/java/com/example/hashtaghunter/ProfileActivity.java
@@ -16,14 +16,17 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        logout=(Button)findViewById(R.id.logout);
+        /* logout=(Button)findViewById(R.id.logout);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(ProfileActivity.this,MainActivity.class));
             }
-        });
+        }); */
         String accessToken = "";
+        Intent i = new Intent(getApplicationContext(),ResultActivity.class);
+        startActivity(i);
+
     }
 }
---
+++ app/src/main/java/com/example/hashtaghunter/ResultActivity.java
@@ -0,0 +1,17 @@
+package com.example.hashtaghunter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultActivity extends AppCompatActivity {
+    String accessToken = "EAANAP8yRGn0BAFMZBIEZCWdYuq2TNBdUVzHI298PBd57DrWPBKywFhwD9VJjWFnqxNLPcdkNMyhSPTAzIGbTJyI9";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+    }
+}
